a6618964d00e65c4e6b3281361602606463e29fc
Tim Hamby <timothyhamby13@augustana.edu>
2016-12-15 21:04:59 -0600

    working socket connections

--- AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/MainActivity.java
+++ AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/MainActivity.java
@@ -1,13 +1,47 @@
 package edu.augustana.csc490.androidbrainapp;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button sendButton;
+    public static final int port = 4567;
+    public static final String ip = "192.168.43.154";
+    private SocketConnection mSocketConnection = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        sendButton = (Button) findViewById(R.id.button);
+
+    }
+
+    public void sendMessage(View view) throws Exception{
+        Log.d("sendMessage", "success");
+        mSocketConnection.sendMessage();
+        Toast.makeText(this, "Message Sent", Toast.LENGTH_LONG);
+        Log.d("sendMessage", "message sent");
+    }
+
+    public void connectToSocket(View view) throws Exception{
+        Log.d("connect to socket", "success");
+        mSocketConnection = new SocketConnection(port, ip);
+        Toast.makeText(this, "connection successful", Toast.LENGTH_LONG);
+        Log.d("connect to socket", "connection successful");
     }
 }
---
+++ AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/SocketConnection.java
@@ -0,0 +1,27 @@
+package edu.augustana.csc490.androidbrainapp;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.io.OutputStreamWriter;
+
+
+/**
+ * Created by hamby on 12/15/2016.
+ */
+
+public class SocketConnection {
+    private Socket socket;
+    private OutputStreamWriter os;
+
+    public SocketConnection(int portNum, String ip) throws Exception{
+        socket = new Socket(ip, portNum);
+        os = new OutputStreamWriter(socket.getOutputStream());
+    }
+
+    public void sendMessage() throws IOException {
+        os.write("Message\n");
+        os.flush();
+    }
+}
