804ed1dc3d65c1472ae8f29af529ba1e0b97b19a
Alerik Vi <alerikvi13@augustana.edu>
2016-12-19 19:31:01 -0600

    Added text fields for IP and port. Manual entry is now usable

--- AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/MainActivity.java
+++ AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/MainActivity.java
@@ -3,21 +3,26 @@ package edu.augustana.csc490.androidbrainapp;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.SocketException;
+import static java.lang.Integer.parseInt;
 
 public class MainActivity extends AppCompatActivity {
     private Button sendButton;
-    public static final int port = 4567;
-    public static final String ip = "192.168.43.154";
+//    public static final int port = 4567;
+//    public static final String ip = "192.168.43.154";
+    private EditText editTextAddress;
+    private EditText editTextPort;
+
+    private int portString;
+    private String addressString;
+
     private SocketConnection mSocketConnection = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
         sendButton = (Button) findViewById(R.id.button);
 
+        editTextAddress = (EditText) findViewById(R.id.address);
+        editTextPort = (EditText) findViewById(R.id.port);
+
+        editTextPort.addTextChangedListener(portTW);
+        editTextAddress.addTextChangedListener(addressTW);
     }
 
     public void sendMessage(View view) throws Exception{
@@ -40,8 +50,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void connectToSocket(View view) throws Exception{
         Log.d("connect to socket", "success");
-        mSocketConnection = new SocketConnection(port, ip);
+        mSocketConnection = new SocketConnection(portString, addressString);
         Toast.makeText(this, "connection successful", Toast.LENGTH_LONG);
         Log.d("connect to socket", "connection successful");
     }
+
+    private TextWatcher portTW = new TextWatcher() {
+        //THE INPUT ELEMENT IS ATTACHED TO AN EDITABLE,
+        //THEREFORE THESE METHODS ARE CALLED WHEN THE TEXT IS CHANGED
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try{
+                addressString = s.toString();
+            }catch(NumberFormatException e) {
+
+            }
+        }
+        public void afterTextChanged(Editable s) {
+        }
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    };
+
+    private TextWatcher addressTW = new TextWatcher() {
+        //THE INPUT ELEMENT IS ATTACHED TO AN EDITABLE,
+        //THEREFORE THESE METHODS ARE CALLED WHEN THE TEXT IS CHANGED
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try{
+                portString = parseInt(s.toString()) ;
+            }catch(NumberFormatException e) {
+
+            }
+        }
+        public void afterTextChanged(Editable s) {
+        }
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    };
 }
