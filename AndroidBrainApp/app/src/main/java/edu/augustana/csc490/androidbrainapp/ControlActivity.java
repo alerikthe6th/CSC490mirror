80612982902618d05f12e4db16698ad23daf0751
Tim Hamby <timothyhamby13@augustana.edu>
2017-01-11 15:14:37 -0600

    changed onClick listener to onTouch listener for robot movement

--- AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/ControlActivity.java
+++ AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/ControlActivity.java
@@ -2,8 +2,10 @@ package edu.augustana.csc490.androidbrainapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /*
  * This program sends strings of movement controls to the robot
@@ -25,7 +27,27 @@ public class ControlActivity extends AppCompatActivity {
         BackwardButton = (Button) findViewById(R.id.backward_btn);
         LeftButton = (Button) findViewById(R.id.left_btn);
         RightButton = (Button) findViewById(R.id.right_btn);
+
+        ForwardButton.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_BUTTON_PRESS){
+                    try {
+                        MainActivity.mSocketConnection.sendMessage("Move");
+                    } catch(Exception e){
+                        Toast.makeText(getApplicationContext() , "connection successful", Toast.LENGTH_LONG);
+                    }
+                } else if(event.getAction() == MotionEvent.ACTION_BUTTON_RELEASE){
+                    try {
+                        MainActivity.mSocketConnection.sendMessage("Stop");
+                    } catch(Exception e){
+                        Toast.makeText(getApplicationContext(), "an error occured", Toast.LENGTH_LONG);
+                    }
+                }
+                return true;
+            }
+        });
     }
+
 
     public void forwardMessage(View view) throws Exception{
         MainActivity.mSocketConnection.sendMessage("Move");
