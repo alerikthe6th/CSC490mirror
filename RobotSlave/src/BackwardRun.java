b46c9e892a67bdf6bb3a2f766499a3adf1677eeb
abbythomson <abigailthomson13@augustana.edu>
2017-01-12 18:46:20 -0600

    adding threading, almost working

---
+++ RobotSlave/src/BackwardRun.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import lejos.hardware.motor.Motor;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+
+public class BackwardRun implements Runnable{
+	
+	private MovePilot pilot;
+	private BufferedReader br;
+	
+	public BackwardRun(BufferedReader reader){
+		
+		Wheel wheel1 = WheeledChassis.modelWheel(Motor.A, 81.6).offset(-90);
+		Wheel wheel2 = WheeledChassis.modelWheel(Motor.D, 81.6).offset(90);
+
+		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
+		pilot = new MovePilot(chassis);
+		
+		br = reader;
+	}
+
+	@Override
+	public void run() {
+		
+		try {
+			while(!br.readLine().equalsIgnoreCase("stop")){
+				pilot.travel(-20);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
--- RobotSlave/src/RobotSlave.java
+++ RobotSlave/src/RobotSlave.java
@@ -1,32 +1,58 @@
 
-import lejos.hardware.Bluetooth;
+import lejos.hardware.Bluetooth; 
 import lejos.hardware.Button;
 
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Vector;
 import java.io.BufferedReader;
+import java.io.IOException;
+
 import lejos.hardware.lcd.*;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
-import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.MovePilot;
 import lejos.utility.PilotProps;
 
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 
-public class RobotSlave {
+public class RobotSlave extends Thread {
 
 	public static Vector movmentQ;
+	public static MovePilot pilot;
+	private static ForwardThread ft;
+	private static BackThread bt;
+	private static LeftThread lt;
+	private static RightThread rt;
 
 	public static void main(String[] args) throws Exception {
 		Movement move = new Movement();
 
 		movmentQ = new Vector();
+		
+		EV3LargeRegulatedMotor left = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor right = new EV3LargeRegulatedMotor(MotorPort.C);
+		
+
+		Wheel wheel1 = WheeledChassis.modelWheel(left, 81.6).offset(-90);
+		Wheel wheel2 = WheeledChassis.modelWheel(right, 81.6).offset(90);
+
+		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
+		pilot = new MovePilot(chassis);
+		
+		Vector threads = new Vector();
+		
+		ft = new ForwardThread(pilot);
+		bt = new BackThread(pilot);
+		lt = new LeftThread(pilot);
+		rt = new RightThread(pilot);
 
 		int port = 4567;
 		ServerSocket server = new ServerSocket(port);
@@ -40,12 +66,21 @@ public class RobotSlave {
 			String str = br.readLine();
 
 			while (str != null) {
- /*TODO
-  * look at threads for this
-  */
-				movmentQ.add(str);
-				move.newCommand(str);
-
+				Thread current;
+				
+				System.out.println(str);
+				if (str.equalsIgnoreCase("move")) {
+					System.out.println("HERE");
+					ft.start();
+				} else if (str.equalsIgnoreCase("back")) {
+					bt.start();
+				}
+				
+				System.out.println("thread set");
+			
+				if (str.equalsIgnoreCase("stop")) {
+					
+				}
 				System.out.println(str);
 				str = br.readLine();
 			}
@@ -56,4 +91,73 @@ public class RobotSlave {
 		Button.waitForAnyPress();
 	}
 
+}
+
+class ForwardThread extends Thread {
+	private MovePilot pilot;
+
+	public ForwardThread(MovePilot p) {
+		pilot = p;
+	}
+
+	@Override
+	public void run() {
+		pilot.travel(20);
+
+		if (interrupted()) {
+			return;
+		}
+	}
+}
+
+class BackThread extends Thread {
+	private MovePilot pilot;
+
+	public BackThread(MovePilot p) {
+		pilot = p;
+
+	}
+
+	@Override
+	public void run() {
+		pilot.travel(-20);
+		if (interrupted()) {
+			return;
+		}
+	}
+}
+
+class LeftThread extends Thread {
+	private MovePilot pilot;
+
+	public LeftThread(MovePilot p) {
+
+		pilot = p;
+	}
+
+	@Override
+	public void run() {
+		pilot.arc(0, -10);
+		if (interrupted()) {
+			return;
+		}
+	}
+
+}
+
+class RightThread extends Thread {
+	private MovePilot pilot;
+
+	public RightThread(MovePilot p) {
+
+		pilot = p;
+	}
+
+	@Override
+	public void run() {
+		pilot.arc(0, 10);
+		if(interrupted()){
+			return;
+		}
+	}
 }
