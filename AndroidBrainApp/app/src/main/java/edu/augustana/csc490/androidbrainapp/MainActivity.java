5d7ac7a52f4381068c7c1b6b9e5cf18cd7ac19cf
Tim Hamby <timothyhamby13@augustana.edu>
2017-02-07 20:41:09 -0600

    shared preferences now working, cleaned up code a bit

--- AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/MainActivity.java
+++ AndroidBrainApp/app/src/main/java/edu/augustana/csc490/androidbrainapp/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +23,19 @@ public class MainActivity extends AppCompatActivity {
     public static final String ipDefRobot = "172.20.10.2"; //constant initially set for a default IP address
     public static final String ipDefCam = "255.255.255.255"; //constant for IP address of the camera
 
+    public static final String ROBOT_IP_PREFS = "ip_address_robot";
+    public static final String ROBOT_PORT_PREFS = "port_num_robot";
+    public static final String CAMERA_IP_PREFS = "ip_address_cam";
+    public static final String CAMERA_PORT_PREFS = "port_num_cam";
+
     private EditText etAddressRobot;
     private EditText etPortRobot;
-    private int portNumRobot;
+    private String portNumRobot;
     private String addressStringRobot;
 
     private EditText etAddressCamera;
     private EditText etPortCamera;
-    private int portNumCamera;
+    private String portNumCamera;
     private String addressStringCamera;
 
     public SharedPreferences prefs;
@@ -69,22 +75,10 @@ public class MainActivity extends AppCompatActivity {
         etPortCamera.addTextChangedListener(portTWCamera);
         etAddressCamera.addTextChangedListener(addressTWCamera);
 
-
-
-        //sets the edit text fields if there is a "default" usage in the past
-//        if(prefs.getString("ip_address_robot", null) != null) {
-//            etAddressRobot.setText(prefs.getString("ip_address_robot", null));
-//        }
-//        if(prefs.getString("ip_address_camera", null) != null) {
-//            etAddressCamera.setText(prefs.getString("ip_address_camera", null));
-//        }
-//        if(prefs.getInt("port_num_robot", -1) != -1) {
-//            etPortRobot.setText("" + prefs.getInt("port_num_robot", -1));
-//        }
-//        if(prefs.getInt("port_num_camera", -1) != -1) {
-//            etPortCamera.setText("" + prefs.getInt("port_num_camera", -1));
-//        }
+        //load shared preference values
+        checkPrefs();
     }
+
 
     /**
      * set default IP and Port number with Abby's phone as a wifi hotspot
@@ -92,17 +86,8 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void setDefaultDestination(View view) {
-
-        //robot connection defaults
-        //etAddressRobot.setText(prefs.getString("ip_address_robot", "Not Found"));
-        //etPortRobot.setText(prefs.getString("port_num_robot", "1234"));
-
         etAddressRobot.setText(ipDefRobot);
         etPortRobot.setText(portDefRobot);
-
-        //camera connection defaults
-       // etAddressCamera.setText(prefs.getString("ip_address_camera", "Not Found"));
-       // etPortCamera.setText(prefs.getString("port_num_camera", "1234"));
     }
 
     /**
@@ -113,11 +98,9 @@ public class MainActivity extends AppCompatActivity {
      * @throws Exception
      */
     public void connectToSocket(View view) throws Exception{
-
-        if(portNumRobot > 0 && addressStringRobot != null) {
-            Log.d("values", "" + portNumRobot + ", " + addressStringRobot);
-            mSocketConnectionRobot = new SocketConnection(portNumRobot, addressStringRobot);
-
+        if(valueOf(portNumRobot)> 0 && addressStringRobot != null) {
+            Log.d("values", portNumRobot + ", " + addressStringRobot);
+            mSocketConnectionRobot = new SocketConnection(valueOf(portNumRobot), addressStringRobot);
             //remove later to a different button
             Intent intent = new Intent(MainActivity.this, SelectControlsActivity.class);
             startActivity(intent);
@@ -135,38 +118,28 @@ public class MainActivity extends AppCompatActivity {
      * @throws Exception
      */
     public void connectToSocketCamera(View view) throws Exception{
-
-        if(portNumCamera > 0 && addressStringCamera != null) {
-            mSocketConnectionCamera = new SocketConnection(portNumCamera, addressStringCamera);
+        if(valueOf(portNumCamera) > 0 && addressStringCamera != null) {
+            mSocketConnectionCamera = new SocketConnection(valueOf(portNumCamera), addressStringCamera);
             Toast.makeText(this, "connection successful", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, "connection failed, retype the destination fields", Toast.LENGTH_LONG).show();
         }
     }
 
-    //TODO: REMOVE AFTER TESTING
-    /*
-    public void launchNewShit(View view){
-        Intent intent = new Intent(MainActivity.this, SelectControlsActivity.class);
-        startActivity(intent);
-    }
-    */
-
     // text watcher object for converting the edit text field for the portDefRobot string to an integer
     private TextWatcher portTWRobot = new TextWatcher() {
         //THE INPUT ELEMENT IS ATTACHED TO AN EDITABLE,
         //THEREFORE THESE METHODS ARE CALLED WHEN THE TEXT IS CHANGED
-
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             try{
-                portNumRobot = parseInt(s.toString());
-                editor.putString("port_num_robot", "" + portNumRobot);
-                editor.commit();
+                portNumRobot = s.toString();
             }catch(NumberFormatException e) {
-
             }
         }
         public void afterTextChanged(Editable s) {
+            //save new robot port num to sharedPrefs
+            editor.putString(ROBOT_PORT_PREFS, portNumRobot);
+            editor.commit();
         }
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
     };
@@ -176,43 +149,38 @@ public class MainActivity extends AppCompatActivity {
     private TextWatcher addressTWRobot = new TextWatcher() {
         //THE INPUT ELEMENT IS ATTACHED TO AN EDITABLE,
         //THEREFORE THESE METHODS ARE CALLED WHEN THE TEXT IS CHANGED
-
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             try{
                 addressStringRobot = s.toString();
-                editor.putString("ip_address_robot", addressStringRobot);
-                editor.commit();
             }catch(NumberFormatException e) {
-
             }
         }
         public void afterTextChanged(Editable s) {
+            //save new robot ip to sharedPrefs
+            editor.putString(ROBOT_IP_PREFS, addressStringRobot);
+            editor.commit();
         }
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
     };
-
-    /*
-    TODO: FIX THESE WATCHERS BELOW
-
-     */
 
     // text watcher object for converting the edit text field for the portDefRobot string to an integer
     private TextWatcher portTWCamera = new TextWatcher() {
         //THE INPUT ELEMENT IS ATTACHED TO AN EDITABLE,
         //THEREFORE THESE METHODS ARE CALLED WHEN THE TEXT IS CHANGED
-
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             try{
-                portNumCamera = parseInt(s.toString());
-                editor.putString("port_num_camera", "" + portNumCamera);
-                editor.commit();
+                portNumCamera = s.toString();
             }catch(NumberFormatException e) {
                 e.printStackTrace();
             }
         }
         public void afterTextChanged(Editable s) {
+            //save new port num to sharedPrefs
+            editor.putString(CAMERA_PORT_PREFS, portNumCamera);
+            editor.commit();
         }
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
     };
 
     // text watcher object for passing the string in the ipDefRobot address edit text field to a data field of the
@@ -224,15 +192,39 @@ public class MainActivity extends AppCompatActivity {
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             try{
                 addressStringCamera = s.toString();
-                editor.putString("ip_address_camera", addressStringCamera);
-                editor.commit();
             }catch(NumberFormatException e) {
-
             }
         }
         public void afterTextChanged(Editable s) {
+            //save new address in sharedPrefs
+            editor.putString(CAMERA_IP_PREFS, addressStringCamera);
+            editor.commit();
         }
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
     };
 
+    public void checkPrefs(){
+        String temp = null;
+        //check robot ip address
+        temp = prefs.getString(ROBOT_IP_PREFS, null);
+        if(temp != null) {
+            etAddressRobot.setText(temp);
+        } else
+        //check camera device ip address
+        temp = prefs.getString(CAMERA_IP_PREFS, null);
+        if(temp != null) {
+            etAddressCamera.setText(temp);
+        }
+        //check robot port number
+        temp = prefs.getString(ROBOT_PORT_PREFS, null);
+        if(temp != null) {
+            etPortRobot.setText(temp);
+        }
+        //check camera device port number
+        temp = prefs.getString(CAMERA_PORT_PREFS, null);
+        if(temp != null) {
+            etPortCamera.setText(temp);
+        }
+    }
 }
